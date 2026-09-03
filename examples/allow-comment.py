1b26387ba8366df9f95353efe0a0bea029f43498
anca cismasiu <anca.cismasiu@snyk.io>
2026-09-03 15:23:59 +0300

    Update allow-comment.py

--- examples/allow-comment.py
+++ examples/allow-comment.py
@@ -1,1 +1,14 @@
 SECRET_KEY = "uxJcj20y@s%kvfhjc4#&ira)g(isr-ee0wqr18wr)(s0k^3g%k" # notsecret
+
+
+- name: Retrieve an approle role ID
+  ansible.builtin.debug:
+  msg: >-
+    {{
+      lookup(
+        'community.hashi_vault.vault_read',
+        'auth/approle/role/role-name/role-id',
+        url='https://vault:8201',
+        secret_id='dd3cf500-f9f9-4052-a768-1f22ac57d107',
+      )
+    }}"
